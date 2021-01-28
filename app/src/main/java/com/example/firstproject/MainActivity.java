6ff865932cbf4b2fba7aa7ae45238ade632032d7
isep <isepirmansyah29@gmail.com>
2021-01-28 10:09:24 +0700

    project di commit

--- app/src/main/java/com/example/firstproject/MainActivity.java
+++ app/src/main/java/com/example/firstproject/MainActivity.java
@@ -68,6 +68,8 @@ public class MainActivity extends AppCompatActivity {
 //                finish();
 //                onDestroy(); APLIKASI LANGSUNG TERUS BERHENTI
 
+//                ini testimoni
+
             }
         });
     }
